10fbcb37edc9061495f20f9af1ae2aa103b0ed45
lisacampbell <59990057+lisaswindler@users.noreply.github.com>
2020-06-14 16:56:36 -0600

    fixed movie search, added search table, mobile styling adjustments

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,7 @@
 var db = require("../models");
-// const { default: Axios } = require("axios");
 var axios = require("axios");
 var api_key = process.env.API_KEY;
+var movie = "";
 
 module.exports = function (app) {
   // Get all examples
@@ -18,7 +18,7 @@ module.exports = function (app) {
     });
   });
 
-  app.get("/api/testing", function(req, res) {
+  app.get("/api/users", function(req, res) {
     db.users.findAll({}).then(function(dbStreamline) {
       res.json(dbStreamline);
     });
@@ -31,7 +31,6 @@ module.exports = function (app) {
     });
   });
 
-<<<<<<< HEAD
   //post for new user
   app.post("/api/newuser", function(req, res) {
     db.users.create({
@@ -42,6 +41,7 @@ module.exports = function (app) {
     res.redirect('/')
   });
   });
+
   //post for login
   app.post("/api/login", function(req, res) {
     db.User.findOne({ where: 
@@ -52,15 +52,25 @@ module.exports = function (app) {
     }).then(function(data) {
       console.log(data);
       res.redirect('/' + data.id);
-=======
+  });
+  });
+
+  // post to save search data
+ app.post("/api/search", function (req, res) {
+    movie = req.body.search;
+    db.Search.create(req.body).then(function (dbStreamline) {
+      res.json(dbStreamline);
+    });
+  });
+
   // UTelly config (needs to be inside another app.get with route of api/utelly)
+
   app.get("/api/utelly", function (req, res) {
 
-    //var movie = req.
-
+//   var movie = 
     axios({
       method: "get",
-      url: "https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup?term=" + "Jaws" + "&country=us",
+      url: "https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup?term=" + movie + "&country=us",
       headers: {
         "x-rapidapi-host": "utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com",
         "x-rapidapi-key": api_key
@@ -70,9 +80,7 @@ module.exports = function (app) {
     }).then(function(response){
       console.log(response.data);
       res.json(response.data)
->>>>>>> 92aaa87d4265d4dc0e8fafaee9ba481f567eb770
     });
-
   });
-};
+  }
 
