16a031a6a0d820440c8b97b03ab76dc06e7c6c72
Dark Knight <jordanccall@gmail.com>
2020-06-14 20:59:49 -0600

    fixed the styling for the newuser page to match the login page

--- models/streamline_table.js
+++ models/streamline_table.js
@@ -31,6 +31,7 @@ module.exports = function (sequelize, DataTypes) {
         }
     });
     
+    //lines 35 to 42 are to help join this table with the user table it is blocked because it will not let the server run at this point.
     //  Streamline.associate = function(models){
     //      Streamline.hasMany(models.User, {
     //         through: "Movies",
